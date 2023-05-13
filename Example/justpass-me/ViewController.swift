fc8654fd33dfead250333a25cc371f1aed7133f0
sameh-amwal <100665288+sameh-amwal@users.noreply.github.com>
2023-05-13 09:07:56 +0200

    reduce minimum deployment target to 10

--- Example/justpass-me/ViewController.swift
+++ Example/justpass-me/ViewController.swift
@@ -16,18 +16,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func Login(_ sender: UIButton) {
-        Task { @MainActor in
-            do {
-                guard let window = self.view.window else { fatalError("The view was not in the app's view hierarchy!") }
-                let JustPassMeClient = JustPassMeClient(presentationAnchor: window);
-                let result = try await JustPassMeClient.authenticate(authenticationURL: "https://europe-west3-justpass-me-sdk-example.cloudfunctions.net/ext-justpass-me-oidc/authenticate/");
-                print("Response Data: \(result)")
-            } catch {
-                print("\(error.localizedDescription)")
-                let errorAlert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                errorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(errorAlert, animated: true, completion: nil)
+        if #available(iOS 16.0, *) {
+            Task { @MainActor in
+                do {
+                    guard let window = self.view.window else { fatalError("The view was not in the app's view hierarchy!") }
+                    let JustPassMeClient = JustPassMeClient(presentationAnchor: window);
+                    let result = try await JustPassMeClient.authenticate(authenticationURL: "https://europe-west3-justpass-me-sdk-example.cloudfunctions.net/ext-justpass-me-oidc/authenticate/");
+                    print("Response Data: \(result)")
+                } catch {
+                    print("\(error.localizedDescription)")
+                    let errorAlert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                    errorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(errorAlert, animated: true, completion: nil)
+                }
             }
+        } else {
+            // Fallback on earlier versions
         }
     }
     
--- justpass-me.podspec
+++ justpass-me.podspec
@@ -1,12 +1,12 @@
 Pod::Spec.new do |s|
   s.name             = 'justpass-me'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'justpass-me iOS sdk for passkeys'
   s.homepage         = 'https://www.justpass.me'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Amwal Developer' => 'shaer@justpass.me' }
   s.source           = { :git => 'https://github.com/justpass-me/justpass-me-iOS-sdk.git', :tag => s.version.to_s }
-  s.ios.deployment_target = '16.2'
+  s.ios.deployment_target = '10.0'
   s.ios.vendored_frameworks = 'justpass-me/Frameworks/JustPassMeFramework.xcframework'
   s.frameworks = 'UIKit'
   s.swift_versions = '4.0'
