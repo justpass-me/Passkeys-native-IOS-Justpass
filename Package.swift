24afbf22175268039a0b6464056d3a587a11b0d3
Fares <Mahmoudfares9394@gmail.com>
2023-08-07 20:01:15 +0300

    add frameWork ass a dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JustPassMe",
-            dependencies: [],
+            dependencies: [
+                .target(name: "JustPassMeFramework")
+            ],
             path: "Sources",
             resources: [
                 .copy("JustPassMeFramework.xcframework")
